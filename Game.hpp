c7ee986511bf75a95bba5a975c443978316abc78
Taha Munawar <109829503+tahamunawar@users.noreply.github.com>
2023-12-07 16:21:08 +0000

    Code can run on Windows platforms as well

--- Game.hpp
+++ Game.hpp
@@ -1,6 +1,13 @@
-#include <SDL2/SDL.h>
-#include <SDL2_image/SDL_image.h>
-#include <SDL2_ttf/SDL_ttf.h>
+#ifdef __APPLE__
+    #include <SDL2/SDL.h>
+    #include <SDL2_image/SDL_image.h>
+    #include <SDL2_ttf/SDL_ttf.h>
+#else
+    #include <SDL.h>
+    #include <SDL_image.h>
+    #include <SDL_ttf.h>
+#endif
+
 #include <iostream>
 
 enum GameState {
--- unit.hpp
+++ unit.hpp
@@ -1,5 +1,10 @@
-#include <SDL2/SDL.h>
-#include <SDL2_image/SDL_image.h>
+#ifdef __APPLE__
+    #include <SDL2/SDL.h>
+    #include <SDL2_image/SDL_image.h>
+#else
+    #include <SDL.h>
+    #include <SDL_image.h>
+#endif
 #include <iostream>
 using namespace std;
 #pragma once

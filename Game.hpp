c3fe14069541d77e1d82af68cad096b39df25641
Taha Munawar <109829503+tahamunawar@users.noreply.github.com>
2023-12-23 02:57:35 +0500

    Update Game.hpp

--- Game.hpp
+++ Game.hpp
@@ -2,6 +2,7 @@
     #include <SDL2/SDL.h>
     #include <SDL2_image/SDL_image.h>
     #include <SDL2_ttf/SDL_ttf.h>
+    #include <SDL2_mixer/SDL_mixer.h>
 #else
     #include <SDL.h>
     #include <SDL_image.h>
